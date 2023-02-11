fa7fc595f923ef8c3bfc686c2c44a474b5c0c956
Viktor Hetman <vitya.getman.79@gmail.com>
2023-02-11 22:56:13 +0200

    refactoring add func from str to obj

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React, {useState} from 'react';
-import TodoForm from './Components/TodoForm/TodoForm';
-import TodoList from './Components/TodoList/TodoList';
-import TodoActions from './Components/TodoActions/TodoActions';
-import './App.css';
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import TodoForm from './Components/TodoForm/TodoForm'
+import TodoList from './Components/TodoList/TodoList'
+import TodoActions from './Components/TodoActions/TodoActions'
+import './App.css'
 
 function App() {
-
   const [todos, setTodos] = useState([])
 
   const addTodoHandler = (text) => {
-    setTodos([...todos, text])
-  } 
+    const newTodo = {
+      text,
+      isComplited: false,
+      id: uuidv4()
+    }
+    setTodos([...todos, newTodo])
+  }
 
+
+  
   return (
     <div className="App">
-     <h1>Todo App</h1>
-     <TodoForm addTodo={addTodoHandler}/>
-     <TodoList todos={todos}/>
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodoHandler} />
+      <TodoList todos={todos} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
